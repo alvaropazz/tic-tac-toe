6f3a25c183825e84bb46006985754dd4fcbd0216
ugochukwu <ugochukwuomeje1@gmail.com>
2020-04-17 20:26:57 +0100

    move all prints and puts the main.rb file

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,14 +1,6 @@
 class GameBoard
-  def initialize
-    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  end
-
-  def play_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts '-----------'
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts '-----------'
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  def initialize(myboard)
+   @board = myboard
   end
 
   # define which space within the board is selected
