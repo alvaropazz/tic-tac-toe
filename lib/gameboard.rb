e231961478eed9c744c70245ea97b6c3962405e1
A L V A R O <alvarop3000@gmail.com>
2020-04-18 22:29:27 -0500

    move the COMBOS constant to the gameplay class

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -30,39 +30,4 @@ class GameBoard
   def player
     turn_counter.even? ? 'X' : 'O'
   end
-
-  COMBOS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ].freeze
-
-  def won?
-    COMBOS.detect do |combos_child|
-      @board[combos_child[0]] == @board[combos_child[1]] &&
-        @board[combos_child[1]] == @board[combos_child[2]] &&
-        empty_space?(combos_child[0])
-    end
-  end
-
-  def full_board?
-    @board.all? { |space| %w[X O].include? space }
-  end
-
-  def draw?
-    full_board? && !won?
-  end
-
-  def over?
-    won? || draw?
-  end
-
-  def winner
-    @board[won?[0]] if won?
-  end
 end
